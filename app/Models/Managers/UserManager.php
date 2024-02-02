7130dc4b9f34ccd9a0aa52379838d186b035bb97
root <root@raspberrypi>
2024-02-02 14:54:52 +0000

    Fix database config issues

--- app/Models/Managers/UserManager.php
+++ app/Models/Managers/UserManager.php
@@ -14,6 +14,7 @@ class UserManager {
 
 	public function checkLogin(string $login, string $mdp): ?User {
 		$user = $this->userDAO->getByLogin($login);
+		
 		if($user !== null && $this->checkPassword($mdp,$user)){
 			return $user;
 		}
